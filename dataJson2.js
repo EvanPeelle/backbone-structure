0aed46baf1f25b2e919ecf863fd0f42cc192434b
Hack Reactor Students <github@hackreactor.com>
2013-05-26 17:14:08 -0700

    combined the row view and the collection view. refactored data to be an array of objects for sampling within application.

---
+++ dataJson2.js
@@ -0,0 +1,18 @@
+var data = [
+{'Keyword state': 'KeywordAd groupStatusMax. CPCClicksImpressionsCTRAvg. CPCCostAvg. positionConv. (1-per-click)Cost / conv. (1-per-click)Conv. rate (1-per-click)View-through conv.LabelsQuality score'},
+{'enabled1': 'cyvitacyvitaeligible0.30231217510.62%0.1942.973.058.592.16%0 --10'},
+{'enabled2': '[cyvita]cyvitaeligible0.3025351329719.06%0.15380.432.09547.042.13%0 --10'},
+{'enabled3': 'cyvita.comcyvitaeligible0.3063417.65%0.211.252.1800.000.00%0 --10'}
+];
+
+
+
+
+
+
+var data2 = [
+{'Keyword state': 'Keyword', 'Ad group', 'Status', 'Max. CPC', 'Clicks', 'Impressions', 'CTR', 'Avg. CPC', 'Cost', 'Avg. position', 'Conv. (1-per-click)', 'Cost / conv. (1-per-click)', 'Conv. rate (1-per-click)', 'View-through conv.', 'Labels', 'Quality score'},
+{'enabled1': 'cyvita', 'cyvita', 'eligible', '0.30', '231', '2175', '10.62%', '0.19', '42.97', '3.0', '5', '8.59', '2.16%', '0', ' --', '10'},
+{'enabled2': '[cyvita]', 'cyvita', 'eligible', '0.30', '2535', '13297', '19.06%', '0.15', '380.43', '2.09', '54', '7.04', '2.13%', '0', ' --', '10'},
+{'enabled3': 'cyvita.com', 'cyvita', 'eligible', '0.30', '6', '34', '17.65%', '0.21', '1.25', '2.18', '0', '0.00', '0.00%', '0', ' --', '10'}
+];
